a9d868e39230743f05e769abcccd3baceb089756
javierGamer <javierx0962@gmail.com>
2021-03-02 18:13:04 -0300

    a
    a

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ formApp.config(function ($stateProvider, $urlRouterProvider) {
     // url will be (/form/interests)
         .state('form.ubicacion', {
             url: '/ubicacion',
-            templateUrl: 'form-ubicacion.html'
+            templateUrl: 'form-ubicacion.html',
+            controller: 'ubicacionController'
         })
 
         .state('form.payment', {
@@ -153,5 +154,56 @@ formApp.controller('formController', function ($scope, $http, $state) {
     //});
 
 
+
+});
+
+
+
+formApp.controller('ubicacionController', function ($scope, $http, $state) {
+
+  $scope.$watch('$viewContentLoaded', function(){
+
+  	var iRegion = 0;
+  	var htmlRegion = '<option value="sin-region">Seleccione región</option><option value="sin-region">--</option>';
+  	var htmlComunas = '<option value="sin-region">Seleccione comuna</option><option value="sin-region">--</option>';
+
+  	jQuery.each(RegionesYcomunas.regiones, function () {
+  		htmlRegion = htmlRegion + '<option value="' + RegionesYcomunas.regiones[iRegion].NombreRegion + '">' + RegionesYcomunas.regiones[iRegion].NombreRegion + '</option>';
+  		iRegion++;
+  	});
+
+  	jQuery('#regiones').html(htmlRegion);
+  	jQuery('#comunas').html(htmlComunas);
+
+  	jQuery('#regiones').change(function () {
+  		var iRegiones = 0;
+  		var valorRegion = jQuery(this).val();
+  		var htmlComuna = '<option value="sin-comuna">Seleccione comuna</option><option value="sin-comuna">--</option>';
+  		jQuery.each(RegionesYcomunas.regiones, function () {
+  			if (RegionesYcomunas.regiones[iRegiones].NombreRegion == valorRegion) {
+  				var iComunas = 0;
+  				jQuery.each(RegionesYcomunas.regiones[iRegiones].comunas, function () {
+  					htmlComuna = htmlComuna + '<option value="' + RegionesYcomunas.regiones[iRegiones].comunas[iComunas] + '">' + RegionesYcomunas.regiones[iRegiones].comunas[iComunas] + '</option>';
+  					iComunas++;
+  				});
+  			}
+  			iRegiones++;
+  		});
+  		jQuery('#comunas').html(htmlComuna);
+  	});
+  	jQuery('#comunas').change(function () {
+  		if (jQuery(this).val() == 'sin-region') {
+  			alert('selecciones Región');
+  		} else if (jQuery(this).val() == 'sin-comuna') {
+  			alert('seleccione Comuna');
+  		}
+  	});
+  	jQuery('#regiones').change(function () {
+  		if (jQuery(this).val() == 'sin-region') {
+  			alert('seleccione Región');
+  		}
+  	});
+
+  });
 
 });
